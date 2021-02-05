752cea70c95afde01e91c0aa9ba45ddf450add84
Mike Bell <mdb036@gmail.com>
2021-02-05 16:47:07 +0000

    Fix nunchuck files

--- nunchuck.c
+++ nunchuck.c
@@ -1,1 +1,68 @@
-../nunchuck/nunchuck.c
+#include <stdio.h>
+#include "pico/stdlib.h"
+#include "hardware/i2c.h"
+
+// I2C defines
+#define I2C_PORT i2c0
+#define NUN_ADDR 0x52
+
+static uint8_t nunchuck_buf[6];
+static struct repeating_timer nunchuck_timer;
+
+static int64_t nunchuck_read_data(alarm_id_t id, void* user_data)
+{
+  i2c_read_blocking(I2C_PORT, NUN_ADDR, nunchuck_buf, 6, false);
+  return 0;
+}
+
+static bool nunchuck_req_data(struct repeating_timer* t) {
+  const uint8_t req = 0;
+  i2c_write_blocking(I2C_PORT, NUN_ADDR, &req, 1, false);
+
+  add_alarm_in_us(375, nunchuck_read_data, NULL, true);
+  return true;
+}
+
+bool nunchuck_init(uint i2c_sda_pin, uint i2c_scl_pin)
+{
+  i2c_init(I2C_PORT, 100*1000);
+
+  gpio_set_function(i2c_sda_pin, GPIO_FUNC_I2C);
+  gpio_set_function(i2c_scl_pin, GPIO_FUNC_I2C);
+  gpio_pull_up(i2c_sda_pin);
+  gpio_pull_up(i2c_scl_pin);
+
+  const uint8_t cmd[] = { 0xF0, 0x55 };
+  if (i2c_write_blocking(I2C_PORT, NUN_ADDR, cmd, 2, false) != 2) {
+    printf("Init failed\n");
+    return false;
+  }
+  sleep_ms(1);
+  const uint8_t cmd2[] = { 0xFB, 0x00 };
+  if (i2c_write_blocking(I2C_PORT, NUN_ADDR, cmd2, 2, false) != 2) {
+    printf("Init failed\n");
+    return false;
+  }
+  sleep_ms(1);
+
+  add_repeating_timer_ms(20, nunchuck_req_data, NULL, &nunchuck_timer);
+
+  return true;
+}
+
+int nunchuck_joyx() {
+  return (int)nunchuck_buf[0] - 128;
+}
+
+int nunchuck_joyy() {
+  return (int)nunchuck_buf[1] - 128;
+}
+
+bool nunchuck_zbutton() {
+  return !(nunchuck_buf[5] & 1);
+}
+
+bool nunchuck_cbutton() {
+  return !(nunchuck_buf[5] & 2);
+}
+
--- nunchuck.h
+++ nunchuck.h
@@ -1,1 +1,17 @@
-../nunchuck/nunchuck.h
+// This nunchuck library was put together using info Arduino nunchuck libraries:
+// https://github.com/todbot/wiichuck_adapter/tree/master/firmware/WiichuckDemo
+// and https://github.com/madhephaestus/WiiChuck
+//
+
+// Nunchuck uses I2C to communicate.  The library uses IC20 hardware
+// resource so the pins specified must be ones the I2C0 can be mapped to
+// and I2C0 can't be used by anything else at the same time.
+//
+// You can set which I2C resource is used at the top of nunchuck.c
+bool nunchuck_init(uint i2c_sda_pin, uint i2c_scl_pin);
+
+// Read current state, joystick values are in the range (-128,127)
+int nunchuck_joyx();
+int nunchuck_joyy();
+bool nunchuck_zbutton();
+bool nunchuck_cbutton();
